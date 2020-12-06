0a8c3a6215e97a901fbd32d2232376ca65e2d251
fawad2aria <fawad3moh@gmail.com>
2020-12-06 10:41:37 -0800

    new update

--- week3/w3d2/number_to_phrase.py
+++ week3/w3d2/number_to_phrase.py
@@ -36,21 +36,26 @@ tens = {
     9: 'ninty'
 }
 def number_pharse(number_input):
-    a = number_input//100
+    if number_input == 0: return 'zero'
+    # a = number_input//100
     # d = (number_input%100)//10
     c = number_input % 10
-    b = (number_input % 100) / 10
-    # a = ((number_input%1000)-(b*10)-c)/100
+    b = ((number_input % 100)-c) / 10
+    a = ((number_input%1000)-(b*10)-c)/100
+
+    
     
     t = ''
     h = ''
     
     if b == 0 and c == 0:
-        t = ones[a] + " hundred "
-    elif b != 0: 
+        t = ones[a] + " hundred "     
+    elif b != 0:        
         t = ones[a] + "hundred and " 
     if b <= 1: 
-        h = ones[number_input%100] 
+        h = ones[number_input%100]
+        # if number_input >=10 and number_input <=19:
+        #     print(teens[number_input])    
     elif b > 1: 
         h = tens[b] + ones[c] 
     st = t + h 
@@ -62,27 +67,13 @@ def number_pharse(number_input):
 #     if b < 1: 
 #         j = ones[c]
 #     return j
-
-
-
-
 def main():
 
     while True:
         number_input = int(input('Enter your number to turn to English word: '))
-        c = number_input % 10
-        b = (number_input % 100) / 10
-        if b < 1: 
-            print(ones[c])
-        elif number_input >=10 and number_input <=19:
-            print(teens)
+        # c = number_input % 10
+        # b = (number_input % 100) / 10 
+       
         # if number_input == 0 - 9:
         print(number_pharse(number_input))
-
-
-
-
-
-
-
 main()
