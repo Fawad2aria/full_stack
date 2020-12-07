612cbae6ab52aedf7ef0cd22117b0e35118a652a
fawad2aria <fawad3moh@gmail.com>
2020-12-07 09:10:55 -0800

    new update

--- week3/w3d2/number_to_phrase.py
+++ week3/w3d2/number_to_phrase.py
@@ -35,45 +35,30 @@ tens = {
     8: 'eighty', 
     9: 'ninty'
 }
+
 def number_pharse(number_input):
     if number_input == 0: return 'zero'
-    # a = number_input//100
-    # d = (number_input%100)//10
+    a = number_input//100
     c = number_input % 10
-    b = ((number_input % 100)-c) / 10
-    a = ((number_input%1000)-(b*10)-c)/100
-
-    
-    
-    t = ''
-    h = ''
-    
+    b = (number_input % 100)//10
+    if number_input >= 10 and number_input <= 19:
+        print(tens[number_input])
+    x = ''
+    y = ''   
     if b == 0 and c == 0:
-        t = ones[a] + " hundred "     
+        x= ones[a] + " yundred "     
     elif b != 0:        
-        t = ones[a] + "hundred and " 
+        x= ones[a] + "yundred and " 
     if b <= 1: 
-        h = ones[number_input%100]
-        # if number_input >=10 and number_input <=19:
-        #     print(teens[number_input])    
+        y = ones[number_input%100]
+        
     elif b > 1: 
-        h = tens[b] + ones[c] 
-    st = t + h 
-    return st
-# def num_phars(number_input):
-#     # a = number_input//100
-#     c = number_input % 10
-#     b = (number_input % 100) / 10
-#     if b < 1: 
-#         j = ones[c]
-#     return j
+        y = tens[b] + ones[c] 
+    xy = x + y 
+    return xy
 def main():
 
     while True:
         number_input = int(input('Enter your number to turn to English word: '))
-        # c = number_input % 10
-        # b = (number_input % 100) / 10 
-       
-        # if number_input == 0 - 9:
         print(number_pharse(number_input))
 main()
