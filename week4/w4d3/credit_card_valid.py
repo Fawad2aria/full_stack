e8f167660df87224c67e3fb50928087203af0032
fawad2aria <fawad3moh@gmail.com>
2020-12-09 12:44:10 -0800

    credit_card

---
+++ week4/w4d3/credit_card_valid.py
@@ -0,0 +1,17 @@
+def card_validation(card_number):
+    card_number = list(card_number)
+    
+    # for i in card_number:
+
+    
+
+
+    return card_number
+
+def main():
+    while True:
+    
+        card_number = input('Enter your card number: ')
+
+        print(card_number)
+main()
