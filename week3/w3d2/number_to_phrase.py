2bbf160e8acb46f544adb6b43a6f321976182b62
fawad2aria <fawad3moh@gmail.com>
2020-12-09 12:39:28 -0800

    number_to_phrase

--- week3/w3d2/number_to_phrase.py
+++ week3/w3d2/number_to_phrase.py
@@ -1,64 +1,55 @@
+ones = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+teens = {10: 'ten',11: 'eleven',12: 'twelve',13: 'thirteen',14: 'fourteen',15: 'fifteen',16: 'sixteen',17: 'seventeen',18: 'eighteen',19: 'nineteen'}
+tens = ['', 'ten','twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
 
+def number_phrase(input_num):
+    ones_degit = input_num % 10
+    tens_degit = int((input_num % 100)/10)
+    hundred_degit = int(input_num/100) 
 
-
-ones = {
-    0: '',
-    1: 'one',
-    2: 'two', 
-    3: 'three',
-    4: 'four', 
-    5: 'five',
-    6: 'six', 
-    7: 'seven',
-    8: 'eight',
-    9: 'nine',
-}
-teens = {
-    10: 'ten',
-    11: 'eleven',
-    12: 'twelve',
-    13: 'thirteen',
-    14: 'fourteen',
-    15: 'fifteen',
-    16: 'sixteen',
-    17: 'seventeen',
-    18: 'eighteen',
-    19: 'nineteen',
-}
-tens = {
-    2: 'twenty',
-    3: 'thirty',
-    4: 'forty',
-    5: 'fifity',
-    6: 'sixty', 
-    7: 'seventy',
-    8: 'eighty', 
-    9: 'ninty'
-}
-
-def number_phrase(number_input):
-    if number_input == 0: return 'zero'
-    elif number_input >= 10 and number_input <= 19:
-        return teens[number_input]
-    a = number_input//100
-    c = number_input % 10
-    b = (number_input % 100)//10
-    x = ''
-    y = ''   
-    if b == 0 and c == 0:
-        x = ones[a] + " hundred "     
-    elif b != 0:        
-        x = ones[a] + "hundred and " 
-    if b <= 1: 
-        y = ones[number_input%100]
-        
-    elif b > 1: 
-        y = tens[b] + ones[c]
-    xy = x + y 
-    return xy
+    if input_num >=100:
+        return ones[hundred_degit] + ' hundred ' + tens[tens_degit] + ones[ones_degit]
+    elif input_num > 19:        
+        return tens[tens_degit] + ones[ones_degit]
+    elif input_num >= 10 and input_num <= 19:
+        return teens[input_num]
+    elif input_num >= 1: 
+        return ones[ones_degit]
+    if input_num == 0: return 'zero'
 def main():
-
     while True:
-        number_input = int(input('Enter your number to turn to English word: '))
-        print(number_phrase(number_input))
-main()
+        to_exit = input('press enter to continue or enter "done" to exit: ')
+        if to_exit == "done":
+            print('See you later')
+            break
+        input_num = int(input('Enter your number to turn to English word: '))
+        print(number_phrase(input_num))
+if __name__ == "__main__":
+    main()  
+
+'''
+# Teacher version
+
+ONES = dict(zip(range(10), ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']))
+TENS = dict(zip(range(10), ['', 'ten', 'twenty', 'thirty', 'forty', 'sixty', 'seventy', 'eighty', 'ninety']))
+TEENS = dict(zip(range(10,20), ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixtenn', 'seventeen', 'eighteen', 'nineteen']))
+def n2p(x):
+    ones_digit = x % 10
+    tens_digit = x // 10 if x < 100 else (x % 100) // 10
+    hundreds_digit = x // 100
+
+    hundreds_word = ONES.get(hundreds_digit, '')
+    tens_word = TENS.get(tens_digit, '')
+    ones_word = ONES[ones_digit]
+    if x>100:
+        return f'{hundreds_word}-hundred-{tens_word}-{ones_word}'
+
+    elif x>19:
+        return f'{tens_word}-{ones_word}'
+    
+    elif x>9:
+        return TEENS[x]
+    else:
+        return ones_word
+print(n2p(9))
+'''
