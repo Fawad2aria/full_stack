f4f5e1a1828957c9ae0cadad986827ec68482c6b
fawad2aria <fawad3moh@gmail.com>
2020-12-14 10:15:56 -0800

    newer updates

--- week4/w4d3/credit_card_valid.py
+++ week4/w4d3/credit_card_valid.py
@@ -1,17 +1,17 @@
 def card_validation(card_number):
     card_number = list(card_number)
-    
-    # for i in card_number:
+    for i in range(0,len(card_number)):
+        card_number[i] = int(card_number[i])
 
-    
-
-
-    return card_number
-
-def main():
-    while True:
-    
-        card_number = input('Enter your card number: ')
-
-        print(card_number)
-main()
+    check_digit = card_number.pop(-1)
+    card_number = card_number[::-1]
+    for i in range(0,len(card_number)):
+        if i % 2 == 0:
+            card_number[i] = card_number[i] *2
+    for i in range(0,len(card_number)):
+        if card_number[i] > 9:
+            card_number[i] = (card_number[i] - 9)
+    card_number = sum(card_number)
+    card_number = card_number % 10
+    return card_number == check_digit
+card_validation('45567375868')
