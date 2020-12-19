52569f9772f262a0649ac66ea45f49d03f1a99b3
fawad2aria <fawad3moh@gmail.com>
2020-12-18 21:23:50 -0800

    count_word

---
+++ week4/w4d4/cound_word.py
@@ -0,0 +1,24 @@
+
+f = open('book123.txt')
+contents = f.read()
+contents = contents.lower()
+import string
+# s = 'I $am a !string with punc&^%*tuation!'
+translator = str.maketrans('', '', string.punctuation)
+contents = contents.translate(translator)
+
+contents = contents.split()
+dict = {}
+STOPWORDS = ['i', 'me.', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', "you're", "you've", "you'll", "you'd", 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', "she's", 'her', 'hers', 'herself', 'it', "it's", 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', "that'll", 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', "don't", 'should', "should've", 'now', 'd', 'll', 'm', 'o', 're', 've', 'y', 'ain', 'aren', "aren't", 'couldn', "couldn't", 'didn', "didn't", 'doesn', "doesn't", 'hadn', "hadn't", 'hasn', "hasn't", 'haven', "haven't", 'isn', "isn't", 'ma', 'mightn', "mightn't", 'mustn', "mustn't", 'needn', "needn't", 'shan', "shan't", 'shouldn', "shouldn't", 'wasn', "wasn't", 'weren', "weren't", 'won', "won't", 'wouldn', "wouldn't"]
+for word in contents:
+    if word not in dict:
+        dict[word] = 0
+    if word not in STOPWORDS:
+        dict[word] += 1
+    
+word_tup = []
+word_tup = list(dict.items())
+word_tup.sort(key=lambda tup: tup[1], reverse=True)
+for i in range(min(10, len(word_tup))):
+    print(word_tup[i])
+
